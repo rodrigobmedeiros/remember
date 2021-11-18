71760c46f183b57d26f5798abf79277b7c41854f
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-18 00:23:08 -0300

    Finishing models implementation including a event model to register all reminders events

--- dates/models.py
+++ dates/models.py
@@ -23,8 +23,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=30, help_text="How would you like to be called")
 
-# Contact table let user have more different contacts for different kind of messages.
-# Starting only with whatsapp option, but in the future we can support other platforms.
 class Contact(models.Model):
 
     phone_number = PhoneNumberField()
@@ -35,25 +33,25 @@ class Reminder(models.Model):
 
     date = models.DateField()
     description = models.CharField(max_length=200)
-
-    # Include some default option to define the event type.
-    event_type = models.CharField(max_length=30, choices=EventType.choices, default=EventType.BIRTHDAY)
     related_person_name = models.CharField(max_length=100)
-
-    # Considering that yearly reminders are a specific case of monthly reminders, I only include a boolean to define if a will be send monthly or not.
     monthly_reminder = models.BooleanField(default=False)
     yearly_reminder = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class UserDefinedEventType(models.Model):
+class EventType(models.Model):
 
-    # In case user choose "Other" option for event type, it's possible to include a message that will be stored into this table
-    # is it make sense? =D
-    event_type = models.CharField(max_length=30, help_text="Enter the Event Type")
-    reminder_event = models.ForeignKey(Reminder, on_delete=models.CASCADE)
+    # I keep only this table removing the field from reminder model
+    # Here although we have choices, it's not exactly a constrain so the user can include a custom text
+    # I'm not shure if it's a good practice =D
+    event_type = models.CharField(max_length=30, choices=EventType.choices, default=EventType.BIRTHDAY)
 
-class EventStatus(models.Model):
+    # I'm not shure if I want to delete the event type because I can use that information maybe to improve options in the future.
+    reminder_event = models.ForeignKey(Reminder, on_delete=models.DO_NOTHING)
 
-    # Thinking about that
-    # TODO
-    pass
+class Event(models.Model):
+
+    # it's kind of a log of sent messages, so i want to keep all entries.
+    # In this case would be better put a deleted boolean field on reminder model and
+    # instead of delete a reminder, only mark as deleted.
+    sending_datetime = models.DateTimeField(auto_now_add=True)
+    reminder_event = models.ForeignKey(Reminder, on_delete=models.DO_NOTHING)
