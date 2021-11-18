3742e00e4004b1f32c4a1bf0108d0b6f6b609b4a
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-18 00:28:52 -0300

    Fixing typo painel (portuguese) -> panel (english)

--- remember/urls.py
+++ remember/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('remember-admin-painel/', admin.site.urls),
+    path('remember-admin-panel/', admin.site.urls),
 ]
