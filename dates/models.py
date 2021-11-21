cbd236f5a3142f31a4868b06a699b8b7f274eb64
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-21 17:30:18 -0300

    include event_type as foreign key into reminder model

--- dates/models.py
+++ dates/models.py
@@ -37,22 +37,18 @@ class Reminder(models.Model):
     monthly_reminder = models.BooleanField(default=False)
     yearly_reminder = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_type = models.ForeignKey(EventType, on_delete=models.SET_NULL)
 
 class EventType(models.Model):
 
-    # I keep only this table removing the field from reminder model
-    # Here although we have choices, it's not exactly a constrain so the user can include a custom text
-    # I'm not shure if it's a good practice =D
     event_type = models.CharField(max_length=30, choices=EventType.choices, default=EventType.BIRTHDAY)
 
-    # I'm not shure if I want to delete the event type because I can use that information maybe to improve options in the future.
-    reminder_event = models.ForeignKey(Reminder, on_delete=models.SET_NULL)
+    # I'm thinking about the inclusion of user as foreign key... is it really needed?
+    # Maybe to show all EventType used until certain time...
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Event(models.Model):
 
-    # it's kind of a log of sent messages, so i want to keep all entries.
-    # In this case would be better put a deleted boolean field on reminder model and
-    # instead of delete a reminder, only mark as deleted.
     sending_datetime = models.DateTimeField(auto_now_add=True)
     successfully_sent = models.BooleanField(default=False)
     reminder_event = models.ForeignKey(Reminder, on_delete=models.SET_NULL)
