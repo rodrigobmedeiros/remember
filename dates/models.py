4028772cf29edd6856d7f899cde5b990c15b0fc6
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-22 09:22:25 -0300

    Fixing issues about models: define null parameter to True when needed, change model name to avoid conflicts

--- dates/models.py
+++ dates/models.py
@@ -10,7 +10,7 @@ class MessageType(models.TextChoices):
 
     WHATSAPP = 'Whatsapp'
 
-class EventType(models.TextChoices):
+class EventTypeOptions(models.TextChoices):
 
     BIRTHDAY = 'Birthday'
     WEDDING_ANNIVERSARY = 'Wedding Anniversary'
@@ -29,6 +29,14 @@ class Contact(models.Model):
     message_type = CharField(max_length=30, choices=MessageType.choices, default=MessageType.WHATSAPP)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class EventType(models.Model):
+
+    event_type = models.CharField(max_length=30, choices=EventTypeOptions.choices, default=EventTypeOptions.BIRTHDAY)
+
+    # I'm thinking about the inclusion of user as foreign key... is it really needed?
+    # Maybe to show all EventType used until certain time...
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Reminder(models.Model):
 
     date = models.DateField()
@@ -37,18 +45,10 @@ class Reminder(models.Model):
     monthly_reminder = models.BooleanField(default=False)
     yearly_reminder = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    event_type = models.ForeignKey(EventType, on_delete=models.SET_NULL)
-
-class EventType(models.Model):
-
-    event_type = models.CharField(max_length=30, choices=EventType.choices, default=EventType.BIRTHDAY)
-
-    # I'm thinking about the inclusion of user as foreign key... is it really needed?
-    # Maybe to show all EventType used until certain time...
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_type = models.ForeignKey(EventType, on_delete=models.SET_NULL, null=True)
 
 class Event(models.Model):
 
     sending_datetime = models.DateTimeField(auto_now_add=True)
     successfully_sent = models.BooleanField(default=False)
-    reminder_event = models.ForeignKey(Reminder, on_delete=models.SET_NULL)
+    reminder_event = models.ForeignKey(Reminder, on_delete=models.SET_NULL, null=True)
