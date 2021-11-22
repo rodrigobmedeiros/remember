c278aeddc2054fd95643c660343d4d0c59db6c15
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-22 06:20:28 -0300

    include a mapping to include_reminder view

--- dates/urls.py
+++ dates/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from dates import views
+
 urlpatterns = [
-    path('include_reminder/', 'views.include_reminder', name='include_reminder'),
+    path('include_reminder/', views.include_reminder, name='include_reminder'),
 ]
