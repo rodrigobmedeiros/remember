c9808ab3dd687a36da059e25feb136d6ca257a61
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-23 23:26:25 -0300

    implement the reminder_form view correctly

--- dates/views.py
+++ dates/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import ReminderForm
-from .models import Reminder
-
 # Create your views here.
 
 def add_reminder(request):
 
     if request.method == 'POST':
 
-        reminder_form = ReminderForm(request.POST)
+        reminder_form = ReminderForm(request.POST) 
 
         if reminder_form.is_valid():
+
             reminder_form.save()
             messages.success(request, ('Your reminder was successfully added!'))
-
-        else:
-            messages.error(request, ('Error saving form'))
 
     reminder_form = ReminderForm()
 
