e0a263d9c7777da2cbb59477b8d80008349d7961
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-11-29 22:30:26 -0300

    remove unused import

--- dates/views.py
+++ dates/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import ReminderForm
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
-user = User.objects.all().first()
-
+@login_required
 def add_reminder(request):
 
     if request.method == 'POST':
@@ -18,7 +16,7 @@ def add_reminder(request):
         if reminder_form.is_valid():
 
             reminder_form = reminder_form.save(commit=False)
-            reminder_form.user = user
+            reminder_form.user = request.user
             reminder_form.save()
             messages.success(request, ('Your reminder was successfully added!'))
 
