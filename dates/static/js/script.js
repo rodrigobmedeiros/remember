c4012266ba5da81534863b1e93ee470bbf91e771
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2021-12-10 00:55:35 -0300

    put monthYear definition inside addEventListener

--- dates/static/js/script.js
+++ dates/static/js/script.js
@@ -1,10 +1,14 @@
-var monthYear = document.getElementById("selected-month").textContent;
-var myModal = new bootstrap.Modal("#reminderModal");
+
 
 document.getElementById("calendar-days").addEventListener("click",function(e) {
   if(e.target && e.target.nodeName == "LI") {
+
+    var monthYear = document.getElementById("selected-month").textContent;
+
     console.log(e.target.textContent);
+    console.log(monthYear);
     // TODO invoke the bootstrap modal
+    var myModal = new bootstrap.Modal(document.getElementById('reminderModal'));
     myModal.show();
   }
 });
