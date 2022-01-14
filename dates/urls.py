0507fced0726a0cbdc339ba8385054896e8f67c6
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-01-14 02:33:25 -0300

    include a contact url

--- dates/urls.py
+++ dates/urls.py
@@ -5,7 +5,8 @@ from dates import views
 
 urlpatterns = [
     path('main/', views.main, name='main'),
-    path('profile/', views.profile, name='profile'), 
+    path('profile/', views.profile, name='profile'),
+    path('contact/' views.profile, name='contact'),
     path('reminders/', views.reminders, name='reminders'),
     path('add-reminder', views.add_reminder, name='add-reminder'),
     path('delete-reminder/<int:id>', views.delete_reminder, name='delete-reminder')
