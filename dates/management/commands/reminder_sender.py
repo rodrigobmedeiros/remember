153632ff7666ee65514d989180ea18ff8a12ad92
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-01-27 23:12:17 -0300

    include a script to define a django command line

---
+++ dates/management/commands/reminder_sender.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = 'Send all reminders for an specific day'
+
+    def handle(self, *args, **options):
+
+        print("Test if it's running...")
+
+        # TODO: 
+        # 1 - get current day
+        # 2 - get all reminders that must be send for that day
+        #   2.1 - analyze day if reminder is monthly
+        #   2.2 - verify day and month if reminder is yearly
+        # 3 - Connect with twilio
+        # For each reminder
+        # 4 - get user
+        # 5 - get contact for this user
+        # 6 - create a twilio client with this info
+        # 7 - create a message based on user and reminder
+        # 8 - send messages
