91ed82da22b07b2cac195c6aee946158596359f6
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-01-28 00:57:11 -0300

    reminder filter implementation considering monthly and yearly frequency

--- dates/management/commands/reminder_sender.py
+++ dates/management/commands/reminder_sender.py
@@ -1,4 +1,7 @@
 from django.core.management import BaseCommand
+from django.db.models import Q
+from dates.models import Reminder
+
 import datetime
 
 
@@ -14,13 +17,18 @@ class Command(BaseCommand):
         # 1 - get current day
 
         today = datetime.datetime.now()
-        print(today.day)
-        print(today.month)
-        print(today.year)
 
         # 2 - get all reminders that must be send for that day
         #   2.1 - analyze day if reminder is monthly
         #   2.2 - verify day and month if reminder is yearly
+
+        monthly_condition = Q(monthly_reminder=True, date__day=today.day)
+        yearly_condition = Q(yearly_reminder=True, date__day=today.day, date__month=today.month)
+
+        reminders = Reminder.objects.filter(monthly_condition | yearly_condition)
+
+        print(reminders)
+
         # 3 - Connect with twilio
         # For each reminder
         # 4 - get user
